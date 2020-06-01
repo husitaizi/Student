cfb784d399164b23ceacebe135121c4c6e016c66
chuantai hu <huchu@sheridancollege.ca>
2020-06-01 14:37:55 +0800

    Second commit

--- src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/exercise1/CardTrick.java
@@ -4,29 +4,47 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week2.softwarefundamentals.exercise1;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
+ *
  * @author dancye
+ * @modifier Chuantai Hu Student ID:991602827
  */
-public class CardTrick {
-    
-    public static void main(String[] args)
-    {
-        Card[] magicHand = new Card[7];
-        
-        for (int i=0; i<magicHand.length; i++)
-        {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-        }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
-        //Then report the result here
-    }
-    
+public class CardTrick
+{
+
+   public static void main (String[] args)
+   {
+      Card[] magicHand = new Card[7];
+
+      for (int i = 0; i < magicHand.length; i++) {
+         Card c = new Card();
+         //c.setValue(insert call to random number generator here)
+         c.setValue((int) (Math.random() * 13));
+         //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+         c.setSuit(c.SUITS[(int) (Math.random() * 3)]);
+         magicHand[i] = c;
+      }
+      Card cUser = new Card();
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Enter a card suit:(\"Hearts\", \"Diamonds\", \"Spades\", \"Clubs\" ");
+      cUser.setSuit(scan.nextLine());
+      System.out.println("Enter a card value: ");
+      cUser.setValue(scan.nextInt());
+
+      //insert code to ask the user for Card value and suit, create their card
+      for (int i = 0; i < magicHand.length; i++) {
+         if (cUser == magicHand[i]) {
+            System.out.println("Good luck this time! ");
+         }
+         System.out.println("Sorry, no luck.");
+      }
+
+      // and search magicHand here
+      //Then report the result here
+   }
+
 }
